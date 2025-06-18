6f96e77d9872ebf2a4930fc34788030e821c105b
Dzjanis <roah.thaar.g@gmail.com>
2025-06-18 19:29:55 +0300

    ready to merge

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 import { searchGoods } from './search.js';
-//для примера
-const items = [{ name: 'cofe' }, { name: 'switshots' }, { name: 'pants' }, { name: 'Watch' }, { name: 'screwDriver' }];
+
+//для примера, в eventListener закоменчено обращение к памяти
+const items = [
+  { id: '1', image: 'scr', oldPrice: '123', name: 'cofe' },
+  { id: '1', image: 'scr', oldPrice: '123', name: 'smth' },
+  { id: '1', image: 'scr', oldPrice: '123', name: 'кофта' },
+  { id: '1', image: 'scr', oldPrice: '123', name: 'штаны' },
+  { id: '1', image: 'scr', oldPrice: '123', name: 'screwDriver' },
+];
+//
 
 export const search = document.getElementById('search');
-// let searchValue;
 
+// переводит курсор влево, при фокусе
 search.addEventListener('focus', () => {
   search.classList.remove('text-center');
 });
+
+// сбрасываем поле поиска в исходное состояние при потере фокуса
 search.addEventListener('blur', () => {
   search.classList.add('text-center');
   search.value = '';
 });
 
 search.addEventListener('input', () => {
+  //const cards = JSONparse(localStorage.getItem('cards'));
   console.log('🚀 ~ search.addEventListener ~ searchGoods:', searchGoods(items));
+  // в моем представлении результат должен уйти на отрисовку
 });
--- src/search.js
+++ src/search.js
@@ -1,12 +1,16 @@
 let timer;
 
 export function searchGoods(goods = []) {
-  clearTimeout(timer);
-  timer = setTimeout(() => {
-    let searchValue = search.value.trim().toLowerCase();
-    console.log(goods);
-    const result = goods.filter((item) => item.name.toLowerCase().includes(searchValue));
-    console.log('🚀 ~ timer=setTimeout ~ result:', result);
-    return result;
-  }, 500);
+  if (Array.isArray(goods)) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      let searchValue = search.value.trim().toLowerCase();
+      console.log(goods); // проверить что пришло
+      const result = goods.filter((item) => item.name.toLowerCase().includes(searchValue));
+      console.log('🚀 ~ timer=setTimeout ~ result:', result); // проверить что ушло
+      return result;
+    }, 500);
+  } else {
+    return [];
+  }
 }
