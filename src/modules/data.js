f71a56c5253200d71423b9e41fe5517e848659ad
Arsenka22 <shishakov32@gmail.com>
2025-06-23 19:52:36 +0300

    Revert "Approved final version"
    This reverts commit 40020b83380e6eb5c769392f6dd4230987c3dbb0, reversing
    changes made to 0c09500b5d91f378da3c32153ad380e93be35cfa.

--- src/modules/data.js
+++
@@ -1,7 +0,0 @@
-export const objCards = [
-  { id: 1, image: "image_1.jpg", name: "Сумка", newPrice: 100, oldPrice: 150, discount: 33 },
-  { id: 2, image: "image_2.jpg", name: "Золотой браслет", newPrice: 200, oldPrice: 350, discount: 43 },
-  { id: 3, image: "image_3.jpg", name: "Кожаный ремень", newPrice: 50, oldPrice: 100, discount: 50 },
-  { id: 4, image: "image_4.jpg", name: "Кроссовки", newPrice: 100, oldPrice: 250, discount: 60 },
-  { id: 5, image: "image_5.jpg", name: "Вязаная кофта", newPrice: 75, oldPrice: 95, discount: 21 },
-];
